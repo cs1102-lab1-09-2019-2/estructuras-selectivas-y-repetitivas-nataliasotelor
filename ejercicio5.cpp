c0e7a75a4dd3b0244f0035bad32e0b337598f45a
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-09-03 22:58:33 -0500

    Ejercicio 5 listo!!!

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,6 +1,46 @@
+#include <cmath>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+double CalcularDesviacion (){
+  float desviacion;
+  float A;
+  float B;
+  int i=1;
+  float sumadecuadrados = 0;
+  float suma = 0;
+  int x;
+  
+  for(i=0; i<N+1 ; i++){
+    cout<<"x"<<i<<":"
+    cin>>x;
+
+    }
+
+  for (x=0; <N+1 ; i++){
+    
+        sumadecuadrados = (sumadecuadrados + pow(i, 2));
+        suma = (suma + i);
+    
+
+  A = (1/n) * (sumadecuadrados-1);
+  B = pow((1/n) * (suma-1), 2);  
+  desviacion = sqrt(A - B);
+  std::cout << std::setprecision(3) << desviacion;return 0;
+
+
+}
 
 
 int main() {
+    float N;
+    cout<<"N: ";
+    cin >> N;
 
-    return 0;
-}
+
