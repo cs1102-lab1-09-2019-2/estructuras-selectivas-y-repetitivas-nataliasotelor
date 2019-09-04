b5e6808a817561de8ded7cd7c0a3ddc73fed34ff
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-09-03 22:03:04 -0500

    Ejercicio 6 listo!!!

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+  double medida;
+  int n;
+  cout<<"Ingresar n=";
+  cin>>medida;
+  for (int i=0;i<=(medida-1);i++){
+    for (int i=0;i<=(medida-1);i+=1){
+      n+=1;
+      if((n%2)==0){
+        cout<<"@";
+      }else{
+        cout<<"#";
+      }
+    }
+    cout<<"\n";
+  }
 
-
-int main() {
-
-    return 0;
 }
