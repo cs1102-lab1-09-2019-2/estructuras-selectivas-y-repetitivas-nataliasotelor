270a9b5b667c2cd47b21c11c8243690e69e95246
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-09-03 22:30:50 -0500

    Ejercicio 3 listo!!!

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+using namespace std;
 
-
-int main() {
-
-    return 0;
+int main(){
+    int n,maxi=0;
+    do{
+        cout<<"Ingrese n: ";
+        cin>>n;
+        if(n>maxi) maxi=n;
+    }while(n!=0);
+    cout<<"El valor máximo es: "<<maxi<<endl;
 }
